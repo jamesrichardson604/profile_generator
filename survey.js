3d485728230ee5dab801db6272e6ccc3b58d1962
James Richardson <jamesrichardson604@gmail.com>
2020-11-04 07:41:50 +0000

    w2 d2 Fun Profile Generator

---
+++ survey.js
@@ -0,0 +1,70 @@
+const { WSAEHOSTDOWN } = require('constants');
+const { rm } = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let answers = []
+let output = ''
+
+rl.question('What do you think of Node.js? ', (answer) => {
+
+  answers.push(answer) 
+  
+  rl.question(`What's your name? Nicknames are also acceptable :) `, (answer) => {
+
+    answers.push(answer) 
+
+    rl.question(`What's an activity you like doing? `, (answer) => {
+
+      answers.push(answer) 
+
+      rl.question(`What do you listen to while doing that?  `, (answer) => {
+        
+        answers.push(answer) 
+
+        rl.question(`Which meal is your favourite (eg: dinner, brunch, etc.)  `, (answer) => {
+
+          answers.push(answer) 
+
+          rl.question(`What's your favourite thing to eat for that meal?  `, (answer) => {
+
+            answers.push(answer) 
+
+            rl.question(`Which sport is your absolute favourite?  `, (answer) => {
+
+              answers.push(answer) 
+
+              rl.question(`What is your superpower? In a few words, tell us what you are amazing at!  `, (answer) => {
+ 
+                answers.push(answer) 
+
+                rl.close()
+                output += `${answers[1]} loves listening to ${answers[3]} while doing what they love: ${answers[2]}. They also enjoy ${answers[4]}, especially if they're eating ${answers[5]}. \nAccording to ${answers[1]}, their superpower is ${answers[7]}. Oh, and they have this to say about Node.js: "${answers[0]}".`
+                console.log(output)
+              });
+            });
+          });
+        });
+      });   
+    });  
+  })
+})
+
+
+
+  
+    
+
+
+
+
+
+
+
+
+
+
